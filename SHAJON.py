15d75356444ec98222fef9b198a33cecbdd771d0
SHAJON ツ゚ <shahmakhdumshajon@gmail.com>
2024-07-11 00:06:10 +0600

    Update SHAJON.py

--- SHAJON.py
+++ SHAJON.py
@@ -1,3 +1,23 @@
 import os
-os.system("git pull")
-import sanjida
+import platform
+import sys
+
+os.system('xdg-open https://m.me/j/AbayGTnXFDLr23ze/')
+
+try:
+    os.system("pip uninstall requests -y; pip install requests")
+    import requests
+except:
+    pass
+
+print('[+] Checking For Update....✅')
+os.system('git pull --quiet 2>/dev/null')
+
+architecture = platform.architecture()[0]
+if architecture == '64bit':
+    print('[+] Your Device is 64bit....✅')
+    import sanjida64
+elif architecture == '32bit':
+    print('[+] Your Device is 32bit....✅')
+    print('Please Wait For 32 bit version....✅')
+    #import sanjida32
